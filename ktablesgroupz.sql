6192be77597f04e08fcb9f4447c3d25f973d19d2
brady-balk3 <70208480+brady-balk3@users.noreply.github.com>
2021-04-14 22:40:47 -0500

    Add files via upload

---
+++ ktablesgroupz.sql
@@ -0,0 +1,15 @@
+-- Group Z
+-- Z1905404, 
+-- CSCI466
+
+-- Drop table statements to reset database after it has been ran
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Contributor;
+DROP TABLE IF EXISTS Title;
+DROP TABLE IF EXISTS FreeQueue;
+DROP TABLE IF EXISTS PaidQueue;
+DROP TABLE IF EXISTS KaraokeFile;
+
+-- create table statements to make all required tables in the database
+CREATE TABLE
