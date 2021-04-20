9d5961d6016c9eee960f6526887b364c0d6019bc
brady-balk3 <70208480+brady-balk3@users.noreply.github.com>
2021-04-20 16:39:20 -0500

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -5,4 +5,34 @@
 	
 	
 	//homepage with search box
+	
+	include ('dblogincreds.php');
+	
+	try //connects to database
+	{
+		$dsn = "mysql:host=courses;dbname=z1905404";
+		$pdo = new PDO($dsn, $username, $password);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOexception $e) //handles error exception
+	{
+		echo "Connection to database failed: " . $e->getMessage();
+	}
+
 ?>
+
+<html>
+	<head>
+		<title>Karaoke Song Search</title>
+		<meta charset="UTF-8">
+	</head>
+	<head>
+	<div class="search">
+		<h1 style="text-align:center">Karaoke Search</h1>
+		<form method="POST" action="resultspage.php">
+			<input type="text" name="search" placeholder="Search for songs!">
+			<button type="submit">
+		</form>
+	</div>
+	</head>
+</html>
--- submit.php
+++ submit.php
@@ -5,4 +5,17 @@
 	
 	
 	//submit page
+	
+	include ('dblogincreds.php');
+	
+		try //connects to database
+	{
+		$dsn = "mysql:host=courses;dbname=z1905404";
+		$pdo = new PDO($dsn, $username, $password);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOexception $e) //handles error exception
+	{
+		echo "Connection to database failed: " . $e->getMessage();
+	}
 ?>
