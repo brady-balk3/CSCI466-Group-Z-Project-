f8e105b38da2e18641b071f4392fa9524a3f1de7
joshuabosse <70494580+joshuabosse@users.noreply.github.com>
2021-04-21 05:01:23 +0000

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -30,6 +30,12 @@
 	<div class="search">
 		<h1 style="text-align:center">Karaoke Search</h1>
 		<form method="POST" action="resultspage.php">
+			<input type="radio" id="title" name="searchOPT" value="title"/>
+			<label for="title">Song Title</label>
+			<input type="radio" id="artist" name="searchOPT" value="artist"/>
+			<label for="title">Artist</label>
+			<input type="radio" id="contributor" name="searchOPT" value="contributor"/>
+			<label for="title">Contributor</label><br><br>
 			<input type="text" name="search" placeholder="Search for songs!">
 			<button type="submit">
 		</form>
