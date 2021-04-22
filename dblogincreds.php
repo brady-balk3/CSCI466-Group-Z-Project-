c162cd7f91c7991ddb4c596bf89820e975041983
brady-balk3 <70208480+brady-balk3@users.noreply.github.com>
2021-04-21 22:50:07 -0500

    Add files via upload

--- dblogincreds.php
+++ dblogincreds.php
@@ -1,7 +1,8 @@
 <?php
 	//Brady Balk Login Creds For DB
-	//Z1905404
-	//CSCI466 Group Project
+	//Brady Balk, Joshoua Bosse
+	//Z1905404, Z1878186
+	//CSCI466 Group Project: Group Z
 	$username = 'z1905404';
 	$password = '1999Nov11';
 ?>
