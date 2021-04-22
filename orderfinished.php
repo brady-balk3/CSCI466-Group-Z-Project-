febaf9076965813d843a0f91bfc25f6bb49447de
brady-balk3 <70208480+brady-balk3@users.noreply.github.com>
2021-04-21 21:34:42 -0500

    Add files via upload

--- orderfinished.php
+++ orderfinished.php
@@ -32,24 +32,29 @@ echo "<html>";
 	if((isset($_POST['PaidQueue']) ? $_POST['PaidQueue']:null))
 	{
 		//insert user name into user table
+		$pqname = 
 		$sql = "INSERT INTO User(Name) VALUES(:Name);";
 		$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 		$success = $prepared->execute(array(':Name' => $_POST['Name']));
-		$newpid = $success->fetch(PDO::FETCH_BOTH);
-		if($newpid)
-		{
-			$pid = $newpid[UserID];
-		}
+		
+		
+		$pquid = "SELECT UserID FROM User WHERE Name = ".$success.";";
+		$result = $pdo->query($pquid);
+		$rs = $result->fetch(PDO::FETCH_BOTH);
+		
+		$newpid = $rs[0];
+		
+		print $newpid;
 		
 		//insert into paid queue 
 		$t = time();
 		$sql = "INSERT INTO PQ(UserID, FileID, Time, Playing, Price) VALUES(:UserID, :FileID, :Time, FALSE, :Price);";
 		$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-		$success = $prepared->execute(array(':UserID' => $pid, ':FileID' => $_POST['KaraokeFile'], ':Time' => $t, ':Price' => $_POST['Price']));
+		$success = $prepared->execute(array('UserID' => $newpid,':FileID' => $_POST['KaraokeFile'], ':Time' => $t, ':Price' => $_POST['PaidQueue']));
 		echo "<br></br>";
 		echo "<p>Your song has succesfully been added to the paid queue, or PQ!</p";
 	}
-	else
+	if((isset($_POST['FreeQueue']) ? $_POST['FreeQueue']:null))
 	{
 		//insert user name into user table
 		$sql = "INSERT INTO User(Name) VALUES(:Name);";
@@ -63,7 +68,7 @@ echo "<html>";
 		
 		//insert into paid queue 
 		$t = time();
-		$sql = "INSERT INTO FQ(UserID, FileID, Time, Playing) VALUES(:UserID, :FileID, :Time, FALSE,);";
+		$sql = "INSERT INTO FQ(UserID, FileID, Time, Playing) VALUES(:UserID, :FileID, :Time, FALSE);";
 		$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 		$success = $prepared->execute(array(':UserID' => $fid, ':FileID' => $_POST['KaraokeFile'], ':Time' => $t));
 		echo "<br></br>";
