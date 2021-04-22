a169fd8ce88c96fe3f444e2a61718970dbf112fa
joshuabosse <70494580+joshuabosse@users.noreply.github.com>
2021-04-22 00:28:30 +0000

    Update resultspage.php

--- resultspage.php
+++ resultspage.php
@@ -14,7 +14,8 @@
 		echo "<tr>";
 		foreach ($rows[0] as $key => $value)
 		{
-			echo "<th>$key</th>";
+			if ($key != "FileID")
+				echo "<th>$key</th>";
 		}
 		echo "</tr>";
 			
@@ -23,14 +24,17 @@
 			echo "<tr>";
 			foreach($row as $key => $value)
 			{
-				echo "<td>";
-				if ($key == "Name")
+				if ($key != "FileID")
 				{
-					echo "<form method='POST' action='submit.php'>";
-						echo "<button type='submit' name='fileID' value='$value'>Select</button>";
-					echo "</form>";
+					echo "<td>";
+					if ($key == "Title")
+					{// add option to select a song
+						echo "<form method='POST' action='submit.php'>";
+						echo "<button type='submit' name='FileID' value='$row[FileID]'>Select</button>";
+						echo "</form>";
+					}
+					echo "$value</td>";
 				}
-				echo "$value</td>";
 			}
 			echo "</tr>";
 		}
@@ -47,20 +51,32 @@
 		if ($_POST['searchOPT'] == "title")
 		{
 			echo "Showing songs titled: $_POST[search]";
-			$sql = "SELECT Name FROM Title WHERE Name = '$_POST[search]';";
+			$sql = "SELECT FileID, Title.Name'Title', Artist.Name'Artist', Version
+						FROM KaraokeFile, Title, Artist
+						WHERE KaraokeFile.TitleID = Title.TitleID
+						AND KaraokeFile.ArtistID = Artist.ArtistID
+						AND Title.Name = '$_POST[search]';";
 		}
 		else if ($_POST['searchOPT'] == "artist")
 		{
 			echo "Showing songs by: $_POST[search]";
-			$sql = "SELECT Name FROM Title WHERE TitleID IN
-						(SELECT TitleID FROM KaraokeFile WHERE ArtistID IN
-							(SELECT ArtistID FROM Artist WHERE Name = '$_POST[search]'));";
+			$sql = "SELECT FileID, Title.Name'Title', Artist.Name'Artist', Version
+						FROM KaraokeFile, Title, Artist
+						WHERE KaraokeFile.TitleID = Title.TitleID
+						AND KaraokeFile.ArtistID = Artist.ArtistID
+						AND Artist.Name = '$_POST[search]';";
 		}
 		else if ($_POST['searchOPT'] == "contributor")
 		{
 			echo "Showing songs with a contribution from: $_POST[search]";
-			$sql = "";
-		}	
+			$sql = "SELECT KaraokeFile.FileID, Title.Name'Title', Artist.Name'Artist', Version
+						FROM KaraokeFile, Title, Artist, Contributes, Contributor
+						WHERE KaraokeFile.TitleID = Title.TitleID
+						AND KaraokeFile.ArtistID = Artist.ArtistID
+						AND KaraokeFile.FileID = Contributes.FileID 
+						AND Contributes.ConID = Contributor.ConID
+						AND Contributor.Name = '$_POST[search]';";
+		}
 	
 		// run query
 		$rs = $pdo->query($sql);
