9634a673555a287957bd5cd4d169ea893e4e295d
brady-balk3 <70208480+brady-balk3@users.noreply.github.com>
2022-03-10 12:38:15 -0600

    Delete dblogincreds.php

--- dblogincreds.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//Brady Balk Login Creds For DB
-	//Brady Balk, Joshoua Bosse, Samuel Wells
-	//Z1905404, Z1878186, Z1860087
-	//CSCI466 Group Project Group Z
-	$username = '';
-	$password = '';
-?>
